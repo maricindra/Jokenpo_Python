7a9096f7834070146c75ef905cedc2c0d50a9e02
Marina Cindra <109001097+maricindra@users.noreply.github.com>
2023-10-24 19:37:46 -0300

    2.1- Jokenpô -  com ifs .py

--- 2.1- Jokenpô - ifs .py
+++ 2.1- Jokenpô - ifs .py
@@ -1,42 +1,46 @@
 # JOKENPÔ ( usando IF em if , elif e else)
+# Uso da bibliotece random
 import random
 
-opcoes = "pedra","papel","tesoura"
+#lista armazenando as opções da partida
+opcoes = ["pedra","papel","tesoura"]
 print("Bem vindo ao game Jokenpô!\n")
 
+#Rodada entre do usuário
 rodada_do_jogador = input("Escolha: Pedra , Papel ou Tesoura \n")
+
+#Por meio da biblioteca random, o computador escolheu
 rodada_do_computador = random.choice(opcoes)
 
-print("\n""Você escolheu ",rodada_do_jogador,"\n""O computador escolheu ",rodada_do_computador)
+#Usuario e computador disputando:
+print("\n""Você escolheu ",rodada_do_jogador,"\nO computador escolheu",rodada_do_computador, "\n")
 
+#regras do Jokenpô para resultado: Pedra
 if rodada_do_jogador.lower() == "pedra": 
     if rodada_do_computador == "pedra":
         print("EMPATE!")
-    
     elif rodada_do_computador == "papel":
         print("Você perdeu !")
-
     else: #pc = tesoura
         print("você venceu!") 
 
+#regras do Jokenpô para resultado: Papel
 elif rodada_do_jogador.lower() == "papel": 
     if rodada_do_computador=="papel":
         print("EMPATE!")
-    
     elif  rodada_do_computador =="tesoura":
         print("Você perdeu !")
-
     else: #pc = pedra
         print("você venceu")
 
+#regras do Jokenpô para resultado: Tesoura
 elif rodada_do_jogador.lower() == "tesoura": 
     if rodada_do_computador=="tesoura":
         print("EMPATE!")
-    
     elif  rodada_do_computador == "pedra":
         print("Você perdeu !")
-
     else: #pc = papel
         print("você venceu")
 
+# Orientando novamente o usuário para o menu.
 else: print ("Opa! Você digitou outra coisa... ")
